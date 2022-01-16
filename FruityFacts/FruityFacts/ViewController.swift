6b493f793f84e3575a84049b18429b25a1af9175
Koty Stannard <k.stannard@live.com>
2022-01-16 15:45:59 -0600

    setup tableView

--- FruityFacts/FruityFacts/ViewController.swift
+++ FruityFacts/FruityFacts/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let sampleData = ["John Doe", "Marcus Aurelius", "David Goggins"]
+    
+    var tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
+        
+        setup()
     }
-
-
 }
 
+extension ViewController {
+    
+    fileprivate func setup() {
+        view.backgroundColor = .systemBackground
+        
+        setupTableView()
+    }
+    
+    fileprivate func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "tableView")
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+    
+}
