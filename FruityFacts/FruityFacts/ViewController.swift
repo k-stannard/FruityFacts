7d1d9ebdb657264a6993acf0183bf78ec065d710
Koty Stannard <k.stannard@live.com>
2022-01-16 15:25:36 -0600

    Add project to repository

---
+++ FruityFacts/FruityFacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FruityFacts
+//
+//  Created by Koty Stannard on 1/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
