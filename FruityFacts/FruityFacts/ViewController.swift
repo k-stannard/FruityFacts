1149ada4dc50c66909dd4ad6e4447b87872a44e6
Koty Stannard <k.stannard@live.com>
2022-05-16 13:22:48 -0500

    Populate the view model with more data and present new data with new labels

--- FruityFacts/FruityFacts/ViewController.swift
+++ FruityFacts/FruityFacts/ViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 class ViewController: UIViewController {
 
     var fruits = [Fruit]()
-    
     var fruitViewModels = [FruitCell.ViewModel]()
     
     var tableView = UITableView()
@@ -88,7 +87,7 @@ extension ViewController {
     
     private func configureTableCells(with fruits: [Fruit]) {
         fruitViewModels = fruits.map {
-            FruitCell.ViewModel(name: $0.name, family: $0.family)
+            FruitCell.ViewModel(name: $0.name, family: $0.family, genus: $0.genus, id: $0.id, order: $0.order)
         }
     }
 }
@@ -98,12 +97,18 @@ class FruitCell: UITableViewCell {
     struct ViewModel {
         let name: String
         let family: String
+        let genus: String
+        let id: Int
+        let order: String
     }
     
     let viewModel: ViewModel? = nil
     
     let nameLabel = UILabel()
     let familyLabel = UILabel()
+    let genusLabel = UILabel()
+    let orderLabel = UILabel()
+    let idLabel = UILabel()
     let stackView = UIStackView()
     
     static let reuseid = "FruitCell"
@@ -128,18 +133,16 @@ extension FruitCell {
     
     func setup() {
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        nameLabel.translatesAutoresizingMaskIntoConstraints = false
-        familyLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        nameLabel.text = "Placeholder name"
-        familyLabel.text = "Placeholder family"
         
         stackView.spacing = 10
         stackView.alignment = .center
         stackView.axis = .vertical
         
-        stackView.addArrangedSubview(nameLabel)
-        stackView.addArrangedSubview(familyLabel)
+        [nameLabel, familyLabel, genusLabel, orderLabel, idLabel].forEach { label in
+            label.text = "Placeholder text"
+            stackView.addArrangedSubview(label)
+        }
+        
         contentView.addSubview(stackView)
     }
     
@@ -153,7 +156,10 @@ extension FruitCell {
     }
     
     func configure(with viewModel: ViewModel) {
-        nameLabel.text = viewModel.name
-        familyLabel.text = viewModel.family
+        nameLabel.text = "Name: " + viewModel.name
+        familyLabel.text = "Family: " + viewModel.family
+        genusLabel.text = "Genus: " + viewModel.genus
+        orderLabel.text = "Order: " + viewModel.order
+        idLabel.text = "Id: \(viewModel.id)"
     }
 }
